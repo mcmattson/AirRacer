f67ad8d7f3f6bf0bb79735ee50ad004d0d748716
Matt <83243547+mcmattson@users.noreply.github.com>
2022-11-07 14:54:12 -0700

    Add files via upload

---
+++ Assets/Script/EndMenu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+    public GameObject pausedUI;
+    public GameObject failedUI;
+    public GameObject winUI;
+    public static EndMenu instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            Resume();
+        }
+    }
+    private void Start()
+    {
+        pausedUI.SetActive(false);
+        failedUI.SetActive(false);
+        winUI.SetActive(false);
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pausedUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+        Debug.Log($"Resuming Game...");
+    }
+
+    void Paused()
+    {
+        pausedUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+        Debug.Log($"Pausing Game...");
+    }
+
+    public void Restart()
+    {
+        Debug.Log($"Restarting Game...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Resume();
+    }
+
+    public void Exit()
+    {
+        Debug.Log($"Exiting Game...");
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/Obstacles.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public bool isFailed;
+    public bool isWin;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        isFailed = false;
+        isWin = false;
+    }
+
+    private void Update()
+    {
+        if (isFailed == true)
+        {
+            EndMenu.instance.failedUI.SetActive(true);           
+            Time.timeScale = 0f;
+        }
+        if (isWin == true)
+        {
+            EndMenu.instance.winUI.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+     void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Trigger!");
+        if (collider.gameObject.tag == "Obstacle")
+        {
+            isFailed = true;
+        }
+        if (collider.gameObject.tag == "Win")
+        {
+            isWin = true;
+        }
+        if (collider.gameObject.tag == "Walls")
+        {
+            isFailed = true;
+        }
+    } 
+}
